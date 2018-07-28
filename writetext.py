3dddd049c9a43e4ced648cbd40462b2dd3f3e531
suetex <1653279@tongji.edu.cn>
2018-07-28 10:14:58 +0800

    submit work

--- writetext.py
+++
@@ -1,9 +0,0 @@
-from sys import argv
-script,textfile=argv
-text=open(textfile,'r+')
-line1="the answer to the life,the universe and everything"
-text.write(line1)
-text.write("\n")
-text.write("42")
-
-
