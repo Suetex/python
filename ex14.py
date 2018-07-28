c4e2da15840afe165fb50194667045eeb288b204
suetex <1653279@tongji.edu.cn>
2018-07-28 14:41:46 +0800

    submit work

---
+++ ex14.py
@@ -0,0 +1,20 @@
+from sys import argv
+script,circum_input=argv
+circum_input=float(circum_input)
+
+pi=3.14
+rearth=6371
+rmars=3397
+r_input=circum_input/(2*pi)
+r_input=int(r_input)
+
+if r_input<=6372&r_input>=6370:
+ print "it's earth"
+elif r_input<=3398&r_input>=3396:
+ print"it's mars"
+elif r_input<=6391&r_input>=6351:
+ print"it' close to the earth"
+elif r_input<=3417&r_input>=3377:
+ print"it' close to the mars"
+else:
+ print"the data is neither earth or mars,please check it again"
